2404647a55cdac7d75cd0f1105f23ced8e7b0110
arun bharadwaj <a.bharadwaj@idscan.com>
2018-08-03 22:51:31 +0100

    factorial - adding example for template meta programming

---
+++ templates/template_meta_programming.cpp
@@ -0,0 +1,19 @@
+// class that when compiled will contain the factorial
+// as the static const value!
+template <unsigned int n>
+struct factorial
+{
+  static const unsigned int value = n * factorial<n-1>::value ;
+};
+
+// template specialization - terminate the recursion
+template <>
+struct factorial<0>
+{
+  static const unsigned int  value = 1;
+};
+
+int main() {
+    unsigned int i = factorial<5>::value;
+    return 0;
+}
